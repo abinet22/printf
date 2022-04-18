bd357cd963bf84f8afe81fc4d39ee0cb3e7c1353
Abinet Tefera <abinet22@gmail.com>
2022-04-19 01:42:17 +0300

    printf

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include "main.h"
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,24 +9,5 @@
  */
 int _putchar(char c)
 {
-	static int contador;
-	static char buffer[1024];
-
-	if (c == -1)
-	{
-		contador = 0;
-		return (0);
-	}
-	if (c == -2 || contador == 1024)
-	{
-		write(1, buffer, contador);
-		contador = 0;
-	}
-	if (c != -1 && c != -2)
-	{
-		buffer[contador] = c;
-		contador++;
-		return (1);
-	}
-	return (0);
+	return (write(1, &c, 1));
 }
